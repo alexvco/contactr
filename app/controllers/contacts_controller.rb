f82036bc6482e0cd399e27f86a29cae361dc8d8e
alexv7 <alexvarjabedian7@gmail.com>
2016-09-12 23:48:29 -0400

    rake db:create rake db:migrate and changes to routes model and controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,25 +1,25 @@
 class ContactsController < ApplicationController
-  before_action :set_contact, only: [:show, :edit, :update, :destroy]
 
-  # GET /contacts
-  # GET /contacts.json
-  def index
-    @contacts = Contact.all
-  end
 
-  # GET /contacts/1
-  # GET /contacts/1.json
-  def show
-  end
+
+
+
+
+
+
+
+
+
+
 
   # GET /contacts/new
   def new
     @contact = Contact.new
   end
 
-  # GET /contacts/1/edit
-  def edit
-  end
+
+
+
 
   # POST /contacts
   # POST /contacts.json
@@ -28,38 +28,37 @@ class ContactsController < ApplicationController
 
     respond_to do |format|
       if @contact.save
-        format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
-        format.json { render :show, status: :created, location: @contact }
+        redirect_to contact_saved_path, notice: 'Message was successfully sent. We will get back to you as soon as possible' 
       else
-        format.html { render :new }
-        format.json { render json: @contact.errors, status: :unprocessable_entity }
+        render :new 
       end
     end
   end
 
-  # PATCH/PUT /contacts/1
-  # PATCH/PUT /contacts/1.json
-  def update
-    respond_to do |format|
-      if @contact.update(contact_params)
-        format.html { redirect_to @contact, notice: 'Contact was successfully updated.' }
-        format.json { render :show, status: :ok, location: @contact }
-      else
-        format.html { render :edit }
-        format.json { render json: @contact.errors, status: :unprocessable_entity }
-      end
-    end
+
+  def contact_saved
   end
 
-  # DELETE /contacts/1
-  # DELETE /contacts/1.json
-  def destroy
-    @contact.destroy
-    respond_to do |format|
-      format.html { redirect_to contacts_url, notice: 'Contact was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,9 @@
 class Contact < ApplicationRecord
+  after_create :send_email	
+  validates :firstname, :lastname, :emasl, :message, presence: { message: "You need to fill out all required fields!" }
+
+  private
+
+  def send_email
+  end
 end

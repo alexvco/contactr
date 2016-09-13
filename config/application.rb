eda40ec4e559f69cddca47e8b38ae121da811606
alexv7 <alexvarjabedian7@gmail.com>
2016-09-13 00:38:00 -0400

    configured config/application.rb with regards to action_mailer, and therefore we are not using config/environments/development.rb nor config/environments/production.rb as action_mailer configurations, instead everything is now in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'action_mailer'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,5 +12,25 @@ module Contactr
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    ##============================================================##
+    ## Config action_mailer
+    ##============================================================##
+    # config.action_mailer.default_url_options = { :host => ENV["HOST"] }
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.default :charset => "utf-8"
+    config.action_mailer.default content_type: "text/html"
+    # ActionMailer::Base.default :content_type => "text/html"
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:              "smtp.gmail.com",
+      port:                 587,
+      domain:               'example.com',
+      user_name:            ENV["SENDINBLUE_USERNAME"],
+      password:             ENV["SENDINBLUE_KEY"],
+      authentication:       :plain,
+      enable_starttls_auto: true 
+    }
   end
 end

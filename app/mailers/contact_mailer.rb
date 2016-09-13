5546add902100f2d6769b241c17509d66b2849d6
alexv7 <alexvarjabedian7@gmail.com>
2016-09-13 00:56:21 -0400

    created a method in ContactMailer < ApplicationMailer, created html views and text.erb views

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,2 +1,9 @@
 class ContactMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+   
+   def welcome_email
+      # @user = user
+      # @url  = 'http://www.gmail.com'
+      mail(to: @contact.email, subject: 'Welcome to My Awesome Site')
+   end
 end
